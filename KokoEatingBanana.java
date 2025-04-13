77ee8c3aef0e24e39b14d0dc6b87e69a411daf65
jas04-11 <140260520+jas04-11@users.noreply.github.com>
2025-04-13 18:11:45 +0530

    Create KokoEatingBanana.java

---
+++ KokoEatingBanana.java
@@ -0,0 +1,57 @@
+/*Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109 */
+
+class Solution {
+    public int minEatingSpeed(int[] v, int h) {
+        int low=0;
+        int high=findMax(v);
+        while(low<=high) {
+        int mid= low+(high-low)/2;
+        int reqTime = calculateTotalHours(v, mid);
+        if (reqTime <= h) {
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+    return low;
+    }
+
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = v.length;
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+
+  
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        int n = v.length;
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+}
